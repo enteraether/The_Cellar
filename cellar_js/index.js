9a2ad30865cd1d8a2309e9e6254cd701e80e13af
enteraether <thetheresagarcia@gmail.com>
2020-02-29 15:20:37 -0500

    black boxes and working on filter/switch function

--- cellar_js/index.js
+++ cellar_js/index.js
@@ -6,28 +6,29 @@ const WINESURL = "http://localhost:3000/api/v1/wines"
 document.addEventListener('DOMContentLoaded', () => {
 
     loadBasePage()
+    // listenForHmpgClicks()
     ////////
 })
 
 function loadBasePage(){
-    fetch(BASEURL)
+    fetch(WINESURL)
     .then(resp=>resp.json())
-    .then(renderHomePage)
+    .then(sortWines)
 }
 
-function renderHomePage(wines){
-    const wineCollection = document.getElementById('welcome-image')
-    let whiteWineBox = document.createElement('div')
-    whiteWineBox.className = "wWB"
-    whiteWineBox.innerHTML = `
-    <h4></
-    `
+function sortWines(wines){
+    // const wineCollection = document.getElementById('welcome-image')
+    // let wineBox = document.createElement('div')
+    // wineBox.className = "wine-box"
+    // wineBox.innerHTML = `
+    // <h4>Whites</h4>
+    // `
 
-   let winesByType =  wines.filter(function (e){
-       switch (wineType) {
+   let winesByType =  wines.filter(function(wine){
+       switch (wine.wineType) {
            case "Red":
 
-                console.log("red")
+                console.log(wine.wineType === 'Red')
                break;
        
             // case "White":
@@ -44,3 +45,21 @@ function renderHomePage(wines){
        }
    })
 }
+
+// function listenForHmpgClicks(){
+//     document.addEventListener('click', function(e){
+//     if(e.target.id === 'red-btn'){
+//         loadReds()
+//     }
+// })}
+
+// function loadReds(){
+//     let redsList = document.getElementById('welcome-image')
+//     let redDiv = document.createElement('div')
+
+//     redDiv.className = 'reds-list'
+//     redDiv.innerHTML = `
+    
+//     `
+//     redsList.append(redDiv)
+// }
